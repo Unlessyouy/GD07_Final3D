8e8f8df183d21483d705dcd1b8981a35d57f7aa0
Ben <55573017+muranatsuradio@users.noreply.github.com>
2022-06-12 19:20:15 +0800

    Enemy Ai

--- Assets/Scripts/EnemyComponents/PatrolPathComponent.cs
+++ Assets/Scripts/EnemyComponents/PatrolPathComponent.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PatrolPathComponent : MonoBehaviour
+namespace EnemyComponents
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PatrolPathComponent : MonoBehaviour
     {
-        
-    }
+        private float _radius = 0.5f;
+        private void OnDrawGizmos()
+        {
+            for (var i = 0; i < transform.childCount; i++)
+            {
+                Gizmos.DrawSphere(GetWaypointPosition(i), _radius);
+                Gizmos.DrawLine(GetWaypointPosition(i), GetWaypointPosition(GetNextWaypointIndex(i)));
+            }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public int GetNextWaypointIndex(int i)
+        {
+            var nextIndex = i == transform.childCount - 1 ? 0 : i + 1;
+            return nextIndex;
+        }
+
+        public Vector3 GetWaypointPosition(int i)
+        {
+            return transform.GetChild(i).position;
+        }
     }
 }

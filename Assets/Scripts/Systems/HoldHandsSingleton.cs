f86e2e0e9f284d7448b351fb6fc966b1e0c51e73
MEITIANRIGOU <71801399+MEITIANRIGOU@users.noreply.github.com>
2022-06-30 18:50:48 +0800

    好像改了些什么，又好像没改甚么的改动（来自办公室摸鱼的咖喱

--- Assets/Scripts/Systems/HoldHandsSingleton.cs
+++ Assets/Scripts/Systems/HoldHandsSingleton.cs
@@ -1,69 +1,69 @@
-//using System;
-//using CharacterControl;
-//using UnityEngine;
+using System;
+using CharacterControl;
+using UnityEngine;
 
-//namespace Systems
-//{
-//    public class HoldHandsSingleton : MonoBehaviour
-//    {
-//        [SerializeField] private BasicControl Father;
-//        [SerializeField] private FatherClimbComponent FatherClimbComponent;
-//        [SerializeField] private BasicControl Son;
-//        private float _horizontalInput;
-        
-//        public bool IsHoldingHands { get; set; }
+namespace Systems
+{
+    public class HoldHandsSingleton : MonoBehaviour
+    {
+    //    [SerializeField] private BasicControl Father;
+    //    [SerializeField] private FatherClimbComponent FatherClimbComponent;
+    //    [SerializeField] private BasicControl Son;
+    //    private float _horizontalInput;
 
-//        private bool _isFatherInteract;
-//        private bool _isSonInteract;
-//        private bool _isCloseEnough;
-//        [SerializeField] private float CloseDistance = 2f;
+    //    public bool IsHoldingHands { get; set; }
 
-//        public static HoldHandsSingleton Instance;
+    //    private bool _isFatherInteract;
+    //    private bool _isSonInteract;
+    //    private bool _isCloseEnough;
+    //    [SerializeField] private float CloseDistance = 2f;
 
-//        private void Awake()
-//        {
-//            if (Instance != null && Instance != this)
-//            {
-//                Destroy(gameObject);
-//            }
-//            else
-//            {
-//                Instance = this;
-//            }
-//        }
-        
-//        private void Update()
-//        {
-//            _horizontalInput = Input.GetAxisRaw("Horizontal");
-//            _isFatherInteract = Input.GetButton("HoldHandFather") || Math.Abs(Input.GetAxisRaw("HoldHandFather") - 1) < 0.1f;;
-//            _isSonInteract = Input.GetButton("HoldHandSon") || Math.Abs(Input.GetAxisRaw("HoldHandSon") - 1) < 0.1f;
-//            _isCloseEnough = Vector3.Distance(Father.transform.position, Son.transform.position) <= CloseDistance;
+    //    public static HoldHandsSingleton Instance;
 
-//            IsHoldingHands = _isSonInteract && _isFatherInteract && _isCloseEnough;
-//            Father.IsHoldingHands = IsHoldingHands;
-//            Son.IsHoldingHands = IsHoldingHands;
-//        }
+    //    private void Awake()
+    //    {
+    //        if (Instance != null && Instance != this)
+    //        {
+    //            Destroy(gameObject);
+    //        }
+    //        else
+    //        {
+    //            Instance = this;
+    //        }
+    //    }
 
-//        private void FixedUpdate()
-//        {
-//            if (IsHoldingHands && !Father.isClimbing && !Son.isClimbing)
-//            {
-//                var adjustmentMultiplier = Father.movingSpeed / Son.movingSpeed;
-//                Father.Move(_horizontalInput);
-//                Son.Move(_horizontalInput * adjustmentMultiplier);
-//                return;
-//            }
+    //    private void Update()
+    //    {
+    //        _horizontalInput = Input.GetAxisRaw("Horizontal");
+    //        _isFatherInteract = Input.GetButton("HoldHandFather") || Math.Abs(Input.GetAxisRaw("HoldHandFather") - 1) < 0.1f; ;
+    //        _isSonInteract = Input.GetButton("HoldHandSon") || Math.Abs(Input.GetAxisRaw("HoldHandSon") - 1) < 0.1f;
+    //        _isCloseEnough = Vector3.Distance(Father.transform.position, Son.transform.position) <= CloseDistance;
 
-//            if (Father.controlled && !Father.isClimbing && !FatherClimbComponent.IsHanging)
-//            {
-//                Father.Move(_horizontalInput);
-//                Son.Move(0);
-//            }
-//            if(Son.controlled && !Son.isClimbing)
-//            {
-//                Son.Move(_horizontalInput);
-//                Father.Move(0);
-//            }
-//        }
-//    }
-//}
+    //        IsHoldingHands = _isSonInteract && _isFatherInteract && _isCloseEnough;
+    //        Father.IsHoldingHands = IsHoldingHands;
+    //        Son.IsHoldingHands = IsHoldingHands;
+    //    }
+
+    //    private void FixedUpdate()
+    //    {
+    //        if (IsHoldingHands && !Father.isClimbing && !Son.isClimbing)
+    //        {
+    //            var adjustmentMultiplier = Father.movingSpeed / Son.movingSpeed;
+    //            Father.Move(_horizontalInput);
+    //            Son.Move(_horizontalInput * adjustmentMultiplier);
+    //            return;
+    //        }
+
+    //        if (Father.controlled && !Father.isClimbing && !FatherClimbComponent.IsHanging)
+    //        {
+    //            Father.Move(_horizontalInput);
+    //            Son.Move(0);
+    //        }
+    //        if (Son.controlled && !Son.isClimbing)
+    //        {
+    //            Son.Move(_horizontalInput);
+    //            Father.Move(0);
+    //        }
+    //    }
+    }
+}

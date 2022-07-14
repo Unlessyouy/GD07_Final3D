7e9de18ed1a956a668bf6f5d738fd0e3e55c4097
Zerkovic <961703142@qq.com>
2022-07-14 13:33:52 +0800

    添加一部分音效

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,10 +11,50 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENT_CAVESUB = 2523607441U;
+        static const AkUniqueID AMBIENT_HEAVYWIND = 1253188249U;
+        static const AkUniqueID CAMPFIRE = 1931646578U;
+        static const AkUniqueID FOOT_PLAYER = 1894362767U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SNOW = 787898836U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID ENVIRONMENTS = 3761286811U;
+        static const AkUniqueID ITEMS = 2151963051U;
+        static const AkUniqueID MAIN_CHARACTER = 3381091279U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID SFX = 393239870U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID CAVE = 4122393694U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
